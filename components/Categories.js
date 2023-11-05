4eb88b426d90b9f769ab6b0707402547a736399e
Saurabh <saurabhyadav0613@gmail.com>
2023-11-05 11:58:23 +0530

    first

---
+++ components/Categories.js
@@ -0,0 +1,47 @@
+import { View, Text, ScrollView } from "react-native";
+import React from "react";
+import CategoryCard from "./CategoryCard";
+
+const Categories = () => {
+  return (
+    <ScrollView
+      horizontal
+      showsHorizontalScrollIndicator={false}
+      contentContainerStyle={{
+        paddingTop: 10,
+        paddingHorizontal: 15,
+      }}
+    >
+      {/* Category Card */}
+      <CategoryCard
+        imageUrl="https://images.unsplash.com/photo-1546069901-ba9599a7e63c?auto=format&fit=crop&q=80&w=1000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxleHBsb3JlLWZlZWR8Mnx8fGVufDB8fHx8fA%3D%3D"
+        title="Testing"
+      />
+      <CategoryCard
+        imageUrl="https://images.unsplash.com/photo-1546069901-ba9599a7e63c?auto=format&fit=crop&q=80&w=1000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxleHBsb3JlLWZlZWR8Mnx8fGVufDB8fHx8fA%3D%3D"
+        title="Testing"
+      />
+      <CategoryCard
+        imageUrl="https://images.unsplash.com/photo-1546069901-ba9599a7e63c?auto=format&fit=crop&q=80&w=1000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxleHBsb3JlLWZlZWR8Mnx8fGVufDB8fHx8fA%3D%3D"
+        title="Testing"
+      />
+       <CategoryCard
+        imageUrl="https://images.unsplash.com/photo-1546069901-ba9599a7e63c?auto=format&fit=crop&q=80&w=1000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxleHBsb3JlLWZlZWR8Mnx8fGVufDB8fHx8fA%3D%3D"
+        title="Testing"
+      />
+       <CategoryCard
+        imageUrl="https://images.unsplash.com/photo-1546069901-ba9599a7e63c?auto=format&fit=crop&q=80&w=1000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxleHBsb3JlLWZlZWR8Mnx8fGVufDB8fHx8fA%3D%3D"
+        title="Testing"
+      />
+       <CategoryCard
+        imageUrl="https://images.unsplash.com/photo-1546069901-ba9599a7e63c?auto=format&fit=crop&q=80&w=1000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxleHBsb3JlLWZlZWR8Mnx8fGVufDB8fHx8fA%3D%3D"
+        title="Testing"
+      />
+
+      {/*  */}
+      {/* <Text>Category</Text> */}
+    </ScrollView>
+  );
+};
+
+export default Categories;
---
+++ components/FeaturedRow.js
@@ -0,0 +1,84 @@
+import { View, Text, ScrollView } from "react-native";
+import React from "react";
+import { ArrowRightIcon } from "react-native-heroicons/outline";
+import RestaurantCards from "./RestaurantCards";
+
+const FeaturedRow = ({ id, title, description }) => {
+  return (
+    <View>
+      <View className="mt-4 flex-row items-center justify-center px-4">
+        <Text className="font-bold text-lg">{title}</Text>
+        <ArrowRightIcon color="#00CCBB" />
+      </View>
+      <Text className="text-xs text-gray-500 px-4">{description}</Text>
+      <ScrollView
+        horizontal
+        contentContainerStyle={{
+          paddingHorizontal: 15,
+        }}
+        showsHorizontalScrollIndicator={false}
+        className="pt-4"
+      >
+        {/* Reasturants Cards */}
+        <RestaurantCards
+          id={123}
+          imageUrl={
+            "https://b.zmtcdn.com/data/collections/2e5c28a5fbcb2b35d84c0a498b0e1ba2_1674823998.jpg?fit=around|562.5:360&crop=562.5:360;*,*"
+          }
+          title="Yo Sushi!"
+          rating={4.5}
+          genre="Japanese"
+          address="123 Main St"
+          short_description="This is a Test description"
+          dishes={[]}
+          long={20}
+          lat={0}
+        />
+          <RestaurantCards
+          id={123}
+          imageUrl={
+            "https://b.zmtcdn.com/data/collections/2e5c28a5fbcb2b35d84c0a498b0e1ba2_1674823998.jpg?fit=around|562.5:360&crop=562.5:360;*,*"
+          }
+          title="Yo Sushi!"
+          rating={4.5}
+          genre="Japanese"
+          address="123 Main St"
+          short_description="This is a Test description"
+          dishes={[]}
+          long={20}
+          lat={0}
+        />
+          <RestaurantCards
+          id={123}
+          imageUrl={
+            "https://b.zmtcdn.com/data/collections/2e5c28a5fbcb2b35d84c0a498b0e1ba2_1674823998.jpg?fit=around|562.5:360&crop=562.5:360;*,*"
+          }
+          title="Yo Sushi!"
+          rating={4.5}
+          genre="Japanese"
+          address="123 Main St"
+          short_description="This is a Test description"
+          dishes={[]}
+          long={20}
+          lat={0}
+        />
+          <RestaurantCards
+          id={123}
+          imageUrl={
+            "https://b.zmtcdn.com/data/collections/2e5c28a5fbcb2b35d84c0a498b0e1ba2_1674823998.jpg?fit=around|562.5:360&crop=562.5:360;*,*"
+          }
+          title="Yo Sushi!"
+          rating={4.5}
+          genre="Japanese"
+          address="123 Main St"
+          short_description="This is a Test description"
+          dishes={[]}
+          long={20}
+          lat={0}
+        />
+      </ScrollView>
+    </View>
+  );
+};
+
+export default FeaturedRow;
